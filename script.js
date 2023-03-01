98727f61d0013c87cb1fc4f66b67310d766d0c67
fernandojbcba <benavidez.fernando@hotmail.com>
2023-03-01 06:06:24 -0300

    clase 4

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
   const value = input.value;
-  const list = document.querySelector('[data-list]')
-  const task = document.createElement('li')
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
   task.classList.add("card");
   input.value = "";
-  const content = `<div>
-  <i class="far fa-check-square icon"></i>
+  const taskContent = document.createElement('div');
+  taskContent.appendChild(checkComplete());
+  const content = `
+  ${checkComplete()}
   <span class="task">${value}</span>
-</div>
-<i class="fas fa-trash-alt trashIcon icon"></i>`
-task.innerHTML = content;
+
+<i class="fas fa-trash-alt trashIcon icon"></i>`;
+task.appendChild(taskContent);
+//task.innerHTML = content;
   console.log(content);
   list.appendChild(task);
 };
@@ -22,3 +25,10 @@ console.log(btn);
 
 //Arrow functions o funciones anonimas
 btn.addEventListener('click', createTask);
+
+const checkComplete = () =>{
+  const i = document.createElement('i');
+  i.classList.add=("far fa-check-square icon");
+  i.classList.add=("fa-check-square");
+  i.classList.add=("icon");
+}
